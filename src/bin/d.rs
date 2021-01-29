cbdc9d9183c574f8fe2d30ae1f8d09e927c805ac
masa <masatoshi.sboard@gmail.com>
2021-01-29 21:06:45 +0900

    Done d

--- src/bin/d.rs
+++ src/bin/d.rs
@@ -1,7 +1,7 @@
 #[allow(unused_imports)]
 use proconio::marker::{Bytes, Chars, Usize1};
 use proconio::{fastout, input};
-use std::cmp::max;
+use std::cmp::{max, min};
 #[fastout]
 fn main() {
     input! {
@@ -10,34 +10,29 @@ fn main() {
         k: usize,
         ss: [(usize, usize); n],
     }
-    let mut dp = vec![vec![vec![-1isize; w + 1]; n + 1]; n + 1];
-    dp[0][0][0] = 0;
-    for i in 0..n {
-        for j in 0..=k {
-            for l in 0..=w {
-                if dp[i][j][l] == -1 {
+    let mut dp = vec![vec![std::isize::MAX; 100 * n + 1]; k + 1];
+    dp[0][0] = 0;
+
+    for (a, b) in ss {
+        for prev_k in (0..k).rev() {
+            for prev_v in (0..100 * n).rev() {
+                if prev_v + b > 100 * n {
                     continue;
                 }
-                if j == k {
-                    dp[i + 1][j][l] = max(dp[i + 1][j][l], dp[i][j][l]);
-                } else {
-                    if l + ss[i].0 <= w {
-                        dp[i + 1][j + 1][l + ss[i].0 as usize] = max(
-                            dp[i + 1][j + 1][l + ss[i].0],
-                            dp[i][j][l] + ss[i].1 as isize,
-                        );
-                        dp[i + 1][j][l] = max(dp[i + 1][j][l], dp[i][j][l]);
-                    } else {
-                        dp[i + 1][j][l] = max(dp[i + 1][j][l], dp[i][j][l]);
-                    }
+                if dp[prev_k][prev_v] == std::isize::MAX {
+                    continue;
                 }
+                dp[prev_k + 1][prev_v + b] =
+                    min(dp[prev_k + 1][prev_v + b], dp[prev_k][prev_v] + a as isize);
             }
         }
     }
     let mut ans = 0;
-    for i in 0..w + 1 {
-        for j in 0..n + 1 {
-            ans = max(ans, dp[n][j][i]);
+    for k in 0..k + 1 {
+        for v in 0..100 * n + 1 {
+            if dp[k][v] <= w as isize {
+                ans = max(ans, v);
+            }
         }
     }
     println!("{}", ans);
